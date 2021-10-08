bc34eeae1f5f12a95ab4b9c97e387e0b03c59228
HicaroD <hdanrlley1@gmail.com>
2021-10-08 15:19:58 -0300

    Overall project structure
    I'm able to connect to the IRC server and print the chat on the terminal
    already

---
+++ src/irc_connection.py
@@ -0,0 +1,30 @@
+import socket
+
+BUFFER_SIZE = 2040
+
+class Client:
+    def __init__(self, address : str, port : int):
+        self.address = address
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self):
+        self.socket.connect((self.address, self.port))
+
+    def send_command_to_server(self, command : str, message : str):
+        self.socket.send(f"{command} {message}\r\n".encode())
+
+    def send_oauth_token_to_server(self, oauth_token : str):
+        self.send_command_to_server("PASS", oauth_token)
+
+    def send_nick_to_server(self, nickname : str):
+        self.send_command_to_server("NICK", nickname)
+
+    def join(self, channel_name : str):
+        self.send_command_to_server("JOIN", channel_name)
+
+    def get_data_from_irc_server(self):
+        return self.socket.recv(BUFFER_SIZE)
+
+    def pong_to_server(self):
+        self.send_command_to_server("PONG", ":tmi.twitch.tv")
---
+++ src/main.py
@@ -0,0 +1,47 @@
+from irc_connection import Client
+from dotenv import load_dotenv
+import os
+
+"""
+TODO:
+    [] Connect to IRC server
+    [] Print chat on terminal
+"""
+
+load_dotenv()
+
+ADDRESS = "irc.chat.twitch.tv"
+PORT = 6667
+OAUTH_TOKEN = os.getenv("OAUTH_TOKEN")
+
+class TwitchChat:
+    def __init__(self, nickname : str, channel : str):
+        self.client = Client(ADDRESS, PORT)
+        self.nickname = nickname
+        self.channel = "#" + channel
+
+    def join_channel(self, channel_name : str):
+        self.client.join(channel_name)
+
+    def send_credentials_to_server(self):
+        self.client.send_oauth_token_to_server(OAUTH_TOKEN)
+        self.client.send_nick_to_server(self.nickname)
+
+    def run(self):
+        self.client.connect()
+        self.send_credentials_to_server()
+        self.join_channel(self.channel)
+
+        while 1:
+            unparsed_twitch_chat = self.client.get_data_from_irc_server().decode()
+            print(unparsed_twitch_chat)
+
+            if "PING" in unparsed_twitch_chat:
+                self.client.pong_to_server()
+
+
+def main():
+    chat = TwitchChat("hicaro____", "gaules")
+    chat.run()
+
+main()
