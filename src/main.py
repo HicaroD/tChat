5e8a4330d9457584df47882f38d7457bcb54dcae
HicaroD <hdanrlley1@gmail.com>
2021-11-10 20:18:09 -0300

    Automating oauth token with safe user input
    Using getpass, I'm gonna be using user input to get the oauth token

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from parser import Parser
 from customizer import Customizer
 from colorama import Style
+from config import Configuration
 import os
 
 load_dotenv()
@@ -11,20 +12,20 @@ load_dotenv()
 class TwitchChat:
     ADDRESS = "irc.chat.twitch.tv"
     PORT = 6667
-    OAUTH_TOKEN = os.getenv("OAUTH_TOKEN")
 
-    def __init__(self, nickname : str, channel : str):
+    def __init__(self, nickname : str, channel : str, oauth_token : str):
         self.client = Client(TwitchChat.ADDRESS, TwitchChat.PORT)
         self.nickname = nickname
         self.channel = "#" + channel
         self.parser = Parser()
         self.customizer = Customizer()
+        self.OAUTH_TOKEN = oauth_token
 
     def join_channel(self, channel_name : str):
         self.client.join(channel_name)
 
     def send_credentials_to_server(self):
-        self.client.send_oauth_token_to_server(TwitchChat.OAUTH_TOKEN)
+        self.client.send_oauth_token_to_server(self.OAUTH_TOKEN)
         self.client.send_nick_to_server(self.nickname)
 
     def make_beautiful_printing(self, color : str, user : str, message : str):
@@ -48,14 +49,19 @@ class TwitchChat:
             elif "PING" in unparsed_twitch_chat:
                 self.client.send_pong_to_server()
 
-
 def main():
+    configuration = Configuration()
+
     user_argument_parser = UserArgumentParser()
     args = user_argument_parser.parse_all_arguments()
 
+    if not configuration.config_file_exists():
+        configuration.make_config_file()
+
     nickname = getattr(args, "nickname")
     channel_name = getattr(args, "channel")
+    oauth_token = open(".cfg", "r").read()
 
-    chat = TwitchChat(nickname, channel_name)
+    chat = TwitchChat(nickname, channel_name, oauth_token)
     chat.run()
 main()
