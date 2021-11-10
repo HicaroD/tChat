f7076df77dd3221c66cfa03c3b49db8107ff34ee
HicaroD <hdanrlley1@gmail.com>
2021-11-10 20:20:20 -0300

    Creating a Configuration class to get user inputs for oauth tokens

---
+++ src/config.py
@@ -0,0 +1,14 @@
+import os
+import getpass
+
+class Configuration:
+    def config_file_exists(self) -> bool:
+        return os.path.exists(".cfg")
+
+    def make_config_file(self):
+        with open(".cfg", "x") as f:
+            password = self.ask_for_oauth_token()
+            f.write(password)
+
+    def ask_for_oauth_token(self):
+        return getpass.getpass("Insert your Twitch OAuth Token: ")
