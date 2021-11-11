a5fb12f02d43a6120995176ffb34042d5c3361f4
HicaroD <hdanrlley1@gmail.com>
2021-11-11 17:29:41 -0300

    Deleting new line (\r) at the end of the message

--- src/parser.py
+++ src/parser.py
@@ -3,4 +3,9 @@ class Parser:
         splited_message = message.split(":")
         user = splited_message[1].split("!")[0]
         message = splited_message[2]
+
+        if message.endswith("\r\n"):
+            message = "".join(list(message)[:-2:])
+            return user, message
+
         return user, message
