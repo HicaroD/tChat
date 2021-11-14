aee3c43b17fdb2a577051c65ad4eef99fa98a1ef
HicaroD <hdanrlley1@gmail.com>
2021-11-14 19:03:54 -0300

    Fixing bug when sending links on chat
    The parser was just capturing the first part of the link (generally
    "http").

--- src/parser.py
+++ src/parser.py
@@ -1,11 +1,17 @@
 class Parser:
+    def has_new_line_at_the_end(self, twich_chat_message : str) -> bool:
+        return twich_chat_message.endswith("\r\n")
+
+    def delete_new_line_at_the_end(self, twitch_chat_message : str) -> str:
+        return "".join(list(twitch_chat_message)[:-2:])
+
     def parse_message(self, message : str):
         splited_message = message.split(":")
         user = splited_message[1].split("!")[0]
-        message = splited_message[2]
+        message = "".join(splited_message[2::])
 
-        if message.endswith("\r\n"):
-            message = "".join(list(message)[:-2:])
+        if self.has_new_line_at_the_end(message):
+            message = self.delete_new_line_at_the_end(message)
             return user, message
 
         return user, message
