6ef03e00fc70320fefaa24e134aba55583c22896
HicaroD <hdanrlley1@gmail.com>
2021-11-15 17:59:43 -0300

    Building async code for server connection

--- src/irc_connection.py
+++ src/irc_connection.py
@@ -1,5 +1,5 @@
 import socket
-
+import asyncio
 
 class Client:
     BUFFER_SIZE = 2040
@@ -9,23 +9,23 @@ class Client:
         self.port = port
         self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self):
+    async def connect(self):
         self.irc.connect((self.address, self.port))
 
-    def send_command_to_server(self, command : str, message : str):
+    async def send_command_to_server(self, command : str, message : str):
         self.irc.send(f"{command} {message}\r\n".encode())
 
-    def send_oauth_token_to_server(self, oauth_token : str):
-        self.send_command_to_server("PASS", oauth_token)
+    async def send_oauth_token_to_server(self, oauth_token : str):
+        await self.send_command_to_server("PASS", oauth_token)
 
-    def send_nick_to_server(self, nickname : str):
-        self.send_command_to_server("NICK", nickname)
+    async def send_nick_to_server(self, nickname : str):
+        await self.send_command_to_server("NICK", nickname)
 
-    def join(self, channel_name : str):
-        self.send_command_to_server("JOIN", channel_name)
+    async def join(self, channel_name : str):
+        await self.send_command_to_server("JOIN", channel_name)
 
-    def get_data_from_irc_server_response(self):
+    async def get_data_from_irc_server_response(self):
         return self.irc.recv(Client.BUFFER_SIZE)
 
-    def send_pong_to_server(self):
-        self.send_command_to_server("PONG", ":tmi.twitch.tv")
+    async def send_pong_to_server(self):
+        await self.send_command_to_server("PONG", ":tmi.twitch.tv")
