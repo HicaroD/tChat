f8edcdffc07b4479237d8fdd27464e94259bce7d
HicaroD <hdanrlley1@gmail.com>
2021-11-15 17:59:32 -0300

    Changing code to be async

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from parser import Parser
 from customizer import Customizer
 from config import Configuration
+import asyncio
 import os
 
 load_dotenv()
@@ -31,34 +32,34 @@ class TwitchChat:
         self.customizer = Customizer()
         self.OAUTH_TOKEN = oauth_token
 
-    def join_channel(self, channel_name : str):
-        self.client.join(channel_name)
+    async def join_channel(self, channel_name : str):
+        await self.client.join(channel_name)
 
-    def send_credentials_to_server(self):
-        self.client.send_oauth_token_to_server(self.OAUTH_TOKEN)
-        self.client.send_nick_to_server(self.nickname)
+    async def send_credentials_to_server(self):
+        await self.client.send_oauth_token_to_server(self.OAUTH_TOKEN)
+        await self.client.send_nick_to_server(self.nickname)
 
     def make_beautiful_printing(self, color : str, user : str, message : str):
         print(f"{color}[{user}]{self.customizer.RESET_ANSI_CODE}", end = " ")
         print(f"|| {message}")
 
-    def run(self):
-        self.client.connect()
-        self.send_credentials_to_server()
-        self.join_channel(self.channel)
+    async def run(self):
+        await self.client.connect()
+        await self.send_credentials_to_server()
+        await self.join_channel(self.channel)
 
         while 1:
-            unparsed_twitch_chat_message = self.client.get_data_from_irc_server_response().decode()
+            unparsed_twitch_chat_message = await self.client.get_data_from_irc_server_response()
 
-            if "PRIVMSG" in unparsed_twitch_chat_message:
+            if "PRIVMSG" in unparsed_twitch_chat_message.decode():
                 user_text_color = self.customizer.select_color_for_text()
-                user, message = self.parser.parse_message(unparsed_twitch_chat_message)
+                user, message = self.parser.parse_message(unparsed_twitch_chat_message.decode())
                 self.make_beautiful_printing(user_text_color, user, message)
 
-            elif "PING" in unparsed_twitch_chat_message:
-                self.client.send_pong_to_server()
+            elif "PING" in unparsed_twitch_chat_message.decode():
+                await self.client.send_pong_to_server()
 
-def main():
+async def main():
     configuration = Configuration()
 
     if not configuration.config_file_exists():
@@ -67,5 +68,6 @@ def main():
     nickname, channel_name, oauth_token = parse_args()
 
     chat = TwitchChat(nickname, channel_name, oauth_token)
-    chat.run()
-main()
+    await chat.run()
+
+asyncio.run(main())
