49fa022655309d9aeb766e15656967f8396907c7
HicaroD <hdanrlley1@gmail.com>
2021-12-26 19:45:48 -0300

    Creating Customizer package

---
+++ src/customizer/customizer.py
@@ -0,0 +1,12 @@
+import random
+
+class Customizer:
+    RESET_ANSI_CODE = "\u001b[0m"
+
+    def select_random_color_number(self):
+        return random.randint(30, 37)
+
+    def select_color_for_text(self):
+        random_color_number = self.select_random_color_number()
+        color = f"\u001b[{random_color_number};1m"
+        return color
