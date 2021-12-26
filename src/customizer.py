44399ba6f24e85762bb6843e361caedcc15cf8b4
HicaroD <hdanrlley1@gmail.com>
2021-12-26 19:14:36 -0300

    Removing outdated comments

--- src/customizer.py
+++ src/customizer.py
@@ -7,7 +7,6 @@ class Customizer:
         return random.randint(30, 37)
 
     def select_color_for_text(self):
-        """Select any color from colorama.Fore"""
         random_color_number = self.select_random_color_number()
         color = f"\u001b[{random_color_number};1m"
         return color
