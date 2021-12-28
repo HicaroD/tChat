84b70e3fde92ffbb333dc60a7c9e07b75e511ead
HicaroD <hdanrlley1@gmail.com>
2021-12-28 11:19:15 -0300

    Using black for code formatting in config.py

--- src/config.py
+++ src/config.py
@@ -1,18 +1,23 @@
 import os
 import getpass
 
+
 class InvalidOAuthToken(Exception):
-    """Raised when the oauth token from the user input or .cfg is not valid.
-    """
-    def __init__(self, message="Invalid OAuth token. The token should looks like this: 'oauth:0smq58szgnf58ti7h12382askda31o'"):
+    """Raised when the oauth token from the user input or .cfg is not valid."""
+
+    def __init__(
+        self,
+        message="Invalid OAuth token. The token should looks like this: 'oauth:0smq58szgnf58ti7h12382askda31o'",
+    ):
         self.message = message
         super().__init__(self.message)
+
 
 class Configuration:
     def config_file_exists(self) -> bool:
         return os.path.exists(".cfg")
 
-    def is_a_valid_oauth_token(self, token : str) -> bool:
+    def is_a_valid_oauth_token(self, token: str) -> bool:
         return token.startswith("oauth:")
 
     def make_config_file(self):
@@ -27,7 +32,9 @@ class Configuration:
                     file.write(token.rstrip())
 
         except InvalidOAuthToken:
-            print("You can directly insert your oauth token in the .cfg stored in src/, with no extra spaces in the file")
+            print(
+                "You can directly insert your oauth token in the .cfg stored in src/, with no extra spaces in the file"
+            )
 
     def ask_for_oauth_token(self):
         return getpass.getpass("Insert your Twitch OAuth Token: ")
