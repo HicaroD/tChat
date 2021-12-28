55cd9991638a0f1d45f41d08b015aa01a046a83f
HicaroD <hdanrlley1@gmail.com>
2021-12-28 11:20:47 -0300

    Using black for code formatting

--- src/irc_connection.py
+++ src/irc_connection.py
@@ -1,10 +1,11 @@
 import socket
 import asyncio
 
+
 class Client:
     BUFFER_SIZE = 2040
 
-    def __init__(self, address : str, port : int):
+    def __init__(self, address: str, port: int):
         self.address = address
         self.port = port
         self.irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,16 +13,16 @@ class Client:
     async def connect(self):
         self.irc.connect((self.address, self.port))
 
-    async def send_command_to_server(self, command : str, message : str):
+    async def send_command_to_server(self, command: str, message: str):
         self.irc.send(f"{command} {message}\r\n".encode())
 
-    async def send_oauth_token_to_server(self, oauth_token : str):
+    async def send_oauth_token_to_server(self, oauth_token: str):
         await self.send_command_to_server("PASS", oauth_token)
 
-    async def send_nick_to_server(self, nickname : str):
+    async def send_nick_to_server(self, nickname: str):
         await self.send_command_to_server("NICK", nickname)
 
-    async def join(self, channel_name : str):
+    async def join(self, channel_name: str):
         await self.send_command_to_server("JOIN", channel_name)
 
     async def get_data_from_irc_server_response(self):
