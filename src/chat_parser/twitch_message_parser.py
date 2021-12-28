2299ff09d1d21fa27c413ac04967941b759ac02b
HicaroD <hdanrlley1@gmail.com>
2021-12-28 11:18:07 -0300

    Renaming parser/ package (conflicts)

--- src/chat_parser/twitch_message_parser.py
+++ src/chat_parser/twitch_message_parser.py
@@ -1,14 +1,14 @@
 class Parser:
     @staticmethod
-    def has_new_line_at_the_end(twich_chat_message : str) -> bool:
+    def has_new_line_at_the_end(twich_chat_message: str) -> bool:
         return twich_chat_message.endswith("\r\n")
 
     @staticmethod
-    def delete_new_line_at_the_end(twitch_chat_message : str) -> str:
+    def delete_new_line_at_the_end(twitch_chat_message: str) -> str:
         return "".join(list(twitch_chat_message)[:-2:])
 
     @staticmethod
-    def parse_message(message : str):
+    def parse_message(message: str):
         splited_message = message.split(":")
         user = splited_message[1].split("!")[0]
         message = "".join(splited_message[2::])
