22c5faf4205784eacec5780cd88b0b6b5c5b5a3e
HicaroD <hdanrlley1@gmail.com>
2021-12-31 13:56:11 -0300

    Adding unit tests for the twitch chat message parser

---
+++ tests/test_parser.py
@@ -0,0 +1,28 @@
+import sys, os
+import unittest
+
+sys.path.insert(0, os.path.abspath(".."))
+
+from tChat.chat_parser.twitch_message_parser import Parser
+
+
+class TestTwitchChatParserMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.parser = Parser()
+
+    def test_parser_for_nickname(self):
+        nickname, _ = self.parser.parse_message(
+            ":hicaro____!hicaro____@hicaro____.tmi.twitch.tv PRIVMSG #hicaro____ :testing"
+        )
+        self.assertEqual(nickname, "hicaro____")
+
+    def test_parser_for_user_message(self):
+        _, message = self.parser.parse_message(
+            ":hicaro____!hicaro____@hicaro____.tmi.twitch.tv PRIVMSG #hicaro____ :testing"
+        )
+        self.assertEqual(message, "testing")
+
+
+if __name__ == "__main__":
+    unittest.main()
