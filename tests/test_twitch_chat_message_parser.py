74f74fb3b8c1be37ff0e7a44f897dba5ebe6621f
HicaroD <hdanrlley1@gmail.com>
2022-01-01 15:59:43 -0300

    Adding nickname and message variables to setUp()

--- tests/test_twitch_chat_message_parser.py
+++ tests/test_twitch_chat_message_parser.py
@@ -10,18 +10,15 @@ class TestTwitchChatParserMethods(unittest.TestCase):
 
     def setUp(self):
         self.parser = Parser()
+        self.nickname, self.message = self.parser.parse_message(
+            ":hicaro____!hicaro____@hicaro____.tmi.twitch.tv PRIVMSG #hicaro____ :testing"
+        )
 
     def test_parser_for_nickname(self):
-        nickname, _ = self.parser.parse_message(
-            ":hicaro____!hicaro____@hicaro____.tmi.twitch.tv PRIVMSG #hicaro____ :testing"
-        )
-        self.assertEqual(nickname, "hicaro____")
+        self.assertEqual(self.nickname, "hicaro____")
 
     def test_parser_for_user_message(self):
-        _, message = self.parser.parse_message(
-            ":hicaro____!hicaro____@hicaro____.tmi.twitch.tv PRIVMSG #hicaro____ :testing"
-        )
-        self.assertEqual(message, "testing")
+        self.assertEqual(self.message, "testing")
 
 
 if __name__ == "__main__":
