483ed98356f192516c475bf3dcf762f8450ad312
HicaroD <hdanrlley1@gmail.com>
2022-01-03 12:14:58 -0300

    Turning utility functions into staticmethods

--- src/config/config.py
+++ src/config/config.py
@@ -6,28 +6,33 @@ class Configuration:
     def __init__(self):
         self.config = configparser.ConfigParser()
 
+    @staticmethod
     def config_file_exists(self) -> bool:
         return os.path.exists("./config.ini")
 
+    @staticmethod
     def ask_for_nickname(self):
         return input("Insert your Twitch nickname: ")
 
+    @staticmethod
     def ask_for_oauth_token(self):
         return input("Insert your Twitch OAuth Token:")
 
+    @staticmethod
     def is_a_valid_oauth_token(self, token: str) -> bool:
         return token.startswith("oauth:")
 
+    @staticmethod
     def is_valid_nickname(self, nickname: str):
         return not nickname.isspace() and nickname != ""
 
     def make_config_file(self):
-        nickname = self.ask_for_nickname()
-        oauth_token = self.ask_for_oauth_token()
+        nickname = Configuration.ask_for_nickname()
+        oauth_token = Configuration.ask_for_oauth_token()
 
-        if self.is_valid_nickname(nickname) and self.is_a_valid_oauth_token(
-            oauth_token
-        ):
+        if Configuration.is_valid_nickname(
+            nickname
+        ) and Configuration.is_a_valid_oauth_token(oauth_token):
             self.config["IRC"] = {"nickname": nickname, "oauth_token": oauth_token}
 
             with open("config.ini", "w") as configuration_file:
