d14253f90b5d724eceeae9d66d9aa1aebac558c3
HicaroD <hdanrlley1@gmail.com>
2022-01-03 12:17:54 -0300

    Deleting @staticmethod from non-utility function

--- src/config/config.py
+++ src/config/config.py
@@ -7,10 +7,6 @@ class Configuration:
         self.config = configparser.ConfigParser()
 
     @staticmethod
-    def config_file_exists(self) -> bool:
-        return os.path.exists("./config.ini")
-
-    @staticmethod
     def ask_for_nickname(self):
         return input("Insert your Twitch nickname: ")
 
@@ -25,6 +21,9 @@ class Configuration:
     @staticmethod
     def is_valid_nickname(self, nickname: str):
         return not nickname.isspace() and nickname != ""
+
+    def config_file_exists(self) -> bool:
+        return os.path.exists("./config.ini")
 
     def make_config_file(self):
         nickname = Configuration.ask_for_nickname()
