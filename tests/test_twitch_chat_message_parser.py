a570b69b0067a9ca99d746f2e0e6ee625acf3639
HicaroD <hdanrlley1@gmail.com>
2022-01-03 18:34:02 -0300

    Adding different tests for nickname and message methods

--- tests/test_twitch_chat_message_parser.py
+++ tests/test_twitch_chat_message_parser.py
@@ -10,15 +10,20 @@ class TestTwitchChatParserMethods(unittest.TestCase):
 
     def setUp(self):
         self.parser = Parser()
-        self.nickname, self.message = self.parser.parse_message(
-            ":hicaro____!hicaro____@hicaro____.tmi.twitch.tv PRIVMSG #hicaro____ :testing"
-        )
 
     def test_parser_for_nickname(self):
-        self.assertEqual(self.nickname, "hicaro____")
+        nickname, _ = self.parser.parse_message(
+            ":hicaro____!hicaro____@hicaro____.tmi.twitch.tv PRIVMSG #hicaro____ :testing"
+        )
+        self.assertEqual(nickname, "hicaro____")
+
 
     def test_parser_for_user_message(self):
-        self.assertEqual(self.message, "testing")
+        _, message = self.parser.parse_message(
+            ":hicaro____!hicaro____@hicaro____.tmi.twitch.tv PRIVMSG #hicaro____ :testing : testing message"
+        )
+
+        self.assertEqual(message, "testing : testing message")
 
 
 if __name__ == "__main__":
