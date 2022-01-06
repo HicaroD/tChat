29e8a08edbde27b116f2580b6ce2c70560dc167e
HicaroD <hdanrlley1@gmail.com>
2022-01-06 16:28:37 -0300

    Taking out some utility func from Parser class

--- src/chat_parser/twitch_message_parser.py
+++ src/chat_parser/twitch_message_parser.py
@@ -1,14 +1,14 @@
+def has_new_line_at_the_end(twitch_chat_message: str) -> bool:
+    return twitch_chat_message.endswith("\r\n")
+
+
+def delete_new_line_at_the_end(twitch_chat_message: str) -> str:
+    return "".join(list(twitch_chat_message)[:-2:])
+
+
 class Parser:
     @staticmethod
-    def has_new_line_at_the_end(twitch_chat_message: str) -> bool:
-        return twitch_chat_message.endswith("\r\n")
-
-    @staticmethod
-    def delete_new_line_at_the_end(twitch_chat_message: str) -> str:
-        return "".join(list(twitch_chat_message)[:-2:])
-
-    @staticmethod
-    def extract_data_from_irc_message(unparsed_irc_message : str):
+    def extract_data_from_irc_message(unparsed_irc_message: str):
         unparsed_nickname = unparsed_message = ""
 
         if unparsed_irc_message[0] == ":":
@@ -16,8 +16,12 @@ class Parser:
 
             for character in unparsed_irc_message:
                 if character == ":":
-                    unparsed_nickname = unparsed_irc_message[:unparsed_irc_message.index(character):]
-                    unparsed_message = unparsed_irc_message[unparsed_irc_message.index(character)::]
+                    unparsed_nickname = unparsed_irc_message[
+                        : unparsed_irc_message.index(character) :
+                    ]
+                    unparsed_message = unparsed_irc_message[
+                        unparsed_irc_message.index(character) : :
+                    ]
                     break
 
         return unparsed_nickname, unparsed_message
@@ -26,18 +30,20 @@ class Parser:
     def parse_user_message(unparsed_message: str):
         message = unparsed_message[1::]
 
-        if Parser.has_new_line_at_the_end(message):
-            message = Parser.delete_new_line_at_the_end(message)
+        if has_new_line_at_the_end(message):
+            message = delete_new_line_at_the_end(message)
             return message
 
         return message
 
     @staticmethod
     def parse_nickname(unparsed_nickname: str):
-        return unparsed_nickname.split('!')[0]
+        return unparsed_nickname.split("!")[0]
 
     def get_parsed_twitch_chat_data(self, unparsed_irc_message):
-        unparsed_nickname, unparsed_message = Parser.extract_data_from_irc_message(unparsed_irc_message)
+        unparsed_nickname, unparsed_message = Parser.extract_data_from_irc_message(
+            unparsed_irc_message
+        )
         nickname = Parser.parse_nickname(unparsed_nickname)
         message = Parser.parse_user_message(unparsed_message)
         return nickname, message
