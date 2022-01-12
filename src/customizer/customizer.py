5bf52301a779219726d6b18e072636cad920aeeb
HicaroD <hdanrlley1@gmail.com>
2022-01-12 16:07:07 -0300

    Limiting amount of colors and adding dark background

--- src/customizer/customizer.py
+++ src/customizer/customizer.py
@@ -5,9 +5,10 @@ class Customizer:
     RESET_ANSI_CODE = "\u001b[0m"
 
     def select_random_color_id(self):
-        return random.randint(0, 231)
+        return random.randint(80, 231)
 
     def select_color_for_text(self):
         random_color_id = self.select_random_color_id()
-        color = f"\033[38;5;{random_color_id}m"
+        black_background_color = "\u001b[40;1m"
+        color = black_background_color + f"\033[38;5;{random_color_id}m"
         return color
