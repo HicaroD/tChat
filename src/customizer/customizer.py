c7a54b03d45605423acb17b510a014600dfe434f
HicaroD <hdanrlley1@gmail.com>
2022-01-12 15:51:32 -0300

    Deleting some dark colors
    Doing that because I'm trying to avoid "blank nicknames"

--- src/customizer/customizer.py
+++ src/customizer/customizer.py
@@ -3,10 +3,9 @@ import random
 
 class Customizer:
     RESET_ANSI_CODE = "\u001b[0m"
-    AMOUNT_OF_COLOR_POSSIBILITIES = 255
 
     def select_random_color_id(self):
-        return random.randint(0, Customizer.AMOUNT_OF_COLOR_POSSIBILITIES)
+        return random.randint(0, 231)
 
     def select_color_for_text(self):
         random_color_id = self.select_random_color_id()
