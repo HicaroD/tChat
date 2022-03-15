e8aa314bc28e0e8e0951011900dc2d7b38074d73
HicaroD <hdanrlley1@gmail.com>
2022-03-15 20:04:29 -0300

    Refactor: Deleting utility function
    That function declaration was actually bigger than the definition itself

--- src/chat_parser/twitch_message_parser.py
+++ src/chat_parser/twitch_message_parser.py
@@ -1,7 +1,3 @@
-def has_new_line_at_the_end(twitch_chat_message: str) -> bool:
-    return twitch_chat_message.endswith("\r\n")
-
-
 def delete_new_line_at_the_end(twitch_chat_message: str) -> str:
     return "".join(list(twitch_chat_message)[:-2:])
 
@@ -30,7 +26,7 @@ class Parser:
     def parse_user_message(unparsed_message: str):
         message = unparsed_message[1::]
 
-        if has_new_line_at_the_end(message):
+        if message.endswith("\r\n"):
             message = delete_new_line_at_the_end(message)
             return message
 
@@ -40,7 +36,7 @@ class Parser:
     def parse_nickname(unparsed_nickname: str):
         return unparsed_nickname.split("!")[0]
 
-    def get_parsed_twitch_chat_data(self, unparsed_irc_message):
+    def get_parsed_twitch_chat_data(self, unparsed_irc_message: str):
         unparsed_nickname, unparsed_message = Parser.extract_data_from_irc_message(
             unparsed_irc_message
         )
