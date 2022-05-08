4b64848affae8d697546a85c506733db8bd7f357
HicaroD <hdanrlley1@gmail.com>
2022-05-08 18:36:47 -0300

    [PROJ] changing project structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+long_description = None
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="tChat",
+    description="A Twitch chat UI for your terminal.",
+    long_description=long_description,
+    author="Hícaro",
+    author_email="hdanrlley1@gmail.com",
+    license="MIT License",
+    url="http://pypi.python.org/pypi/tChat/",
+    entry_points = {
+        "console_scripts": [
+            "tchat = tchat.main:main",
+        ]
+    },
+)
+
