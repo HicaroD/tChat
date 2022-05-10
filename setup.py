97fdd61280298d220960a3beb679f4ad595441f5
HicaroD <hdanrlley1@gmail.com>
2022-05-10 10:30:36 -0300

    [DEPLOY] adding classifiers

--- setup.py
+++ setup.py
@@ -8,15 +8,23 @@ setup(
     name="tChat",
     description="A Twitch chat UI for your terminal.",
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="1.0.0",
     author="Hícaro",
     author_email="hdanrlley1@gmail.com",
     license="MIT License",
     url="https://github.com/HicaroD/tChat",
     packages=["tChat"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     entry_points = {
         "console_scripts": [
             "tChat = tChat.main:_main",
         ]
     },
+    python_requires=">=3.6",
 )
 
